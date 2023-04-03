f955dfcdfb43f53dde1a81d6a2fd07f2130fd7ef
Trav563 <trav563@gmail.com>
2023-04-03 13:53:12 -0400

    Add coloring function by changing CSS class to black

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let container = document.querySelector(".container");
+container.innerHTML = "";
+
+container.style.gridTemplateColumns = `repeat(16, 1fr)`;
+container.style.gridTemplateRows = `repeat(16, 1fr)`;
+
+for(i = 0; i < 16; i++){
+    for(j = 0; j < 16; j++){
+        let grid = document.createElement('div');
+        grid.style.border = "1px solid black";
+        container.appendChild(grid);
+
+        grid.addEventListener("mouseover", function(){
+            this.classList.add("black");
+        })
+}
+
+}
+
 function getSize(){
     let gridSize = document.querySelector(".gridSize");
     let h3 = document.querySelector("h3");
@@ -5,6 +24,9 @@ function getSize(){
 
 
     if(gridSize.value < 2 || gridSize.value > 100){
+        if(h4) {
+            h3.removeChild(h4);
+        }
         h4 = document.createElement('h4');
         h4.classList.add(`wrongInput`);
         h4.innerText = "Please select a number between 2 and 100!";
@@ -32,10 +54,15 @@ for(i = 0; i < size; i++){
     const grid = document.createElement('div');
     grid.style.border = "1px solid black";
     container.appendChild(grid);
-    }
+
+    grid.addEventListener("mouseover", function(){
+        this.classList.add("black");
+    })
+}
     
 }
 } 
+
 
 document.addEventListener("DOMContentLoaded", function(){
 })
